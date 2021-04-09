c20f51aa169a0dd0c9e517ac84818ed3da91feb6
jdhal253 <74894546+jdhal253@users.noreply.github.com>
2021-04-09 16:25:29 -0600

    Add files via upload

--- src/model/Toy.java
+++ src/model/Toy.java
@@ -1,5 +1,11 @@
-package model;
+package mru.tsc.model;
 
+/**
+ * This class is the toy superclass that all subclasses inherit from.
+ * @author Jasraj
+ * @author Kevin
+ *
+ */
 public abstract class Toy {
 	
 	public String name;
@@ -9,6 +15,15 @@ public abstract class Toy {
 	public int count;
 	public String age;
 	
+	/**
+	 * This constructs a Toy with a name, serial number, brand, price, count, and appropriate age.
+	 * @param name Name of the toy.
+	 * @param SN Serial number of toy.
+	 * @param brand Brand of toy.
+	 * @param price Price of toy.
+	 * @param count How many toys are in stock.
+	 * @param age Appropriate age for the toy.
+	 */
 	public Toy(String name, String SN, String brand, double price, int count, String age) {
 		this.name = name;
 		this.SN = SN;
@@ -18,59 +33,112 @@ public abstract class Toy {
 		this.age = age;
 	}
 	
+	/**
+	 * Gets the name of the toy.
+	 * @return Returns the name of the toy as a string.
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Sets the name of the toy.
+	 * @param name The name of the toy.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Gets the serial number of the toy.
+	 * @return Returns the serial number of the toy as a string.
+	 */
+	public String getSN() {
+		return SN;
+	}
+	
+	/**
+	 * Sets the serial number of the toy.
+	 * @param SN The serial number of the toy.
+	 */
+	public void setSN(String SN) {
+		this.SN = SN;
+	}
+	
+	/**
+	 * Gets the brand of the toy.
+	 * @return Returns the brand of the toy as a string.
+	 */
+	public String getBrand() {
+		return brand;
+	}
+	
+	/**
+	 * Sets the brand of the toy.
+	 * @param brand The brand of the toy.
+	 */
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	/**
+	 * Gets the price of the toy.
+	 * @return Returns the price of the toy as a double.
+	 */
+	public double getPrice() {
+		return price;
+	}
+	
+	/**
+	 * Sets the price of the toy.
+	 * @param price The price of the toy.
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	/**
+	 * Gets the remaining number of toys.
+	 * @return Returns the remaining number of toys as an int.
+	 */
+	public int getCount() {
+		return count;
+	}
+	
+	/**
+	 * Sets the number of remaining toys.
+	 * @param count The remaining number of toys.
+	 */
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	/**
+	 * Gets the minimum appropriate age for the toy.
+	 * @return Returns the minimum appropriate age of the toy as a string.
+	 */
+	public String getAge() {
+		return age;
+	}
+	
+	/**
+	 * Sets the minimum appropriate age of the toy.
+	 * @param age The minimum appropriate age of the toy.
+	 */
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+
+	/**
+	 * Prints out all relevant toy information.
+	 */
 	@Override
 	public String toString() {
 		return "Toy [name=" + name + ", SN=" + SN + ", brand=" + brand + ", price=" + price + ", count=" + count
 				+ ", age=" + age + "]";
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getSN() {
-		return SN;
-	}
-	
-	public void setSN(String SN) {
-		this.SN = SN;
-	}
-	
-	public String getBrand() {
-		return brand;
-	}
-	
-	public void setBrand(String brand) {
-		this.brand = brand;
-	}
-	
-	public double getPrice() {
-		return price;
-	}
-	
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
-	public void setCount(int count) {
-		this.count = count;
-	}
-	
-	public String getAge() {
-		return age;
-	}
-	
-	public void setAge(String age) {
-		this.age = age;
-	}
 	/**
 	 * @param none
 	 * @return String 
@@ -80,5 +148,4 @@ public abstract class Toy {
 	public String printFormat() {
 		return SN + ";" + name + ";" + brand + ";" + price + ";" + count + ";" + age + ";";
 	}
-	
 }
